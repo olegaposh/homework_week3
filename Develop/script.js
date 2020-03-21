624babee3ecdef905baeebb582e44f134e53c219
Alex Posh <oleg.poshiv@gmail.com>
2020-03-20 20:41:58 -0700

    finished password generator

--- Develop/script.js
+++ Develop/script.js
@@ -4,31 +4,168 @@
 // Click OK to cnfirm including lowercase characters
 // Click Ok to confirm including uppercase characters
 
+// Assignment Code
+
+
 // ask the user the password length
 
-var numberOfCharacters = prompt("How many characters would you like your password to be?(8-128)");
 
-// Make sure user picks a length between 8 and 128
+function generatePassword() {
 
-while (numberOfCharacters < 8 || numberOfCharacters > 128) {
 
-  numberOfCharacters =  prompt("Password has to be between 8 and 128 characters, Try again.");
+  var numberOfCharacters = prompt("How many characters would you like your password to be?(8-128)");
+
+  // Make sure user picks a length between 8 and 128
+
+  while (numberOfCharacters < 8 || numberOfCharacters > 128 || isNaN(numberOfCharacters)) {
+
+    numberOfCharacters = prompt("Password has to be between 8 and 128 characters, Try again.");
+
+  }
+
+  // Ask for password parameters 
+
+  var special = confirm("Click Ok to confirm including special characters");
+  var numeric = confirm("Click OK to confirm including numeric characters");
+  var lowerCase = confirm("Click OK to confirm including lowercase characters");
+  var upperCase = confirm("Click OK to confirm including uppercase characters");
+
+  // user must choose at least one parameter
+
+  while (special === false && numeric === false && lowerCase === false && upperCase === false) {
+
+    alert("You have to confirm at least one password characteristic!");
+    special = confirm("Click Ok to confirm including special characters");
+    numeric = confirm("Click OK to confirm including numeric characters");
+    lowerCase = confirm("Click OK to confirm including lowercase characters");
+    upperCase = confirm("Click OK to confirm including uppercase characters");
+  }
+
+  // declaring arrays
+
+  var specialArray = ["@", "$", "!", "#", "%", "&", "*"];
+  var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
+  var lowerArray = ["a", "b", "c", ",d", "e", "f", "g", "j", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+  //var randomArray = ["2", "j", "F", "&", "4", "y", "#"]; 
+
+
+
+  var master = [];
+  var password = "";
+
+  if (special) {
+
+    for (var i = 0; i < specialArray.length; i++) {
+
+      master.push(specialArray[i]);
+    }
+
+  }
+
+  if (numeric) {
+
+    for (var i = 0; i < numericArray.length; i++) {
+
+      master.push(numericArray[i]);
+    }
+
+  }
+
+  if (lowerCase) {
+
+    for (var i = 0; i < lowerArray.length; i++) {
+
+
+      master.push(lowerArray[i]);
+    }
+
+  }
+
+  if (upperCase) {
+
+    for (var i = 0; i < upperArray.length; i++) {
+
+      master.push(upperArray[i]);
+
+    }
+
+
+  }
+
+  for (var i = 0; i < parseInt(numberOfCharacters); i++) {
+
+    var randomindex = Math.floor(Math.random() * (master.length));
+    password = password + master[randomindex];
+
+  }
+
+  return password;
+}
+
+
+
+/*
+var randomArray = [];
+
+// adding a random requested character
+
+if (special) {
+
+    randomArray.push(randomizer(specialArray)); 
+}
+
+if (numeric) {
+
+  randomArray.push(randomizer(numericArray)); 
+}
+
+if (upperCase) {
+
+  randomArray.push(randomizer(upperArray)); 
+}
+
+if (lowerCase) {
+
+  randomArray.push(randomizer(lowerArray)); 
+}
+
+
+for (i = 0; i < numberOfCharacters.length; i++)   {
+
+  
 
 }
 
-// Ask for password parameters 
-
-var special = confirm("Click Ok to confirm including special characters");
-var numeric = confirm("Click OK to confirm including numeric characters");
-var lowerCase = confirm("Click OK to confirm including lowercase characters");
-var upperCase = confirm("Click OK to confirm including uppercase characters");
-
-// user must choose at least one parameter
 
 
+}
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// function that returns a random index of an array
+
+function randomizer(array) {
+
+
+  var index = Math.floor(Math.random() * array.length);
+  var element = array[index]
+  return element;
+
+}
+
+
+
+// if special is true call randomizer specialArray
+
+
+*/
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -39,5 +176,8 @@ function writePassword() {
 
 }
 
+var generateBtn = document.querySelector("#generate");
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
